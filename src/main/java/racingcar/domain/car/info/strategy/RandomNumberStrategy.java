34884c356b036a23f1e47c5d18a6731f5888fd8a
hongbin23 <khbasd@gmail.com>
2020-06-25 17:21:04 +0900

    refactor: 상수 포장

--- src/main/java/racingcar/domain/car/info/strategy/RandomNumberStrategy.java
+++ src/main/java/racingcar/domain/car/info/strategy/RandomNumberStrategy.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class RandomNumberStrategy implements NumberStrategy {
 	private static final Random random = new Random();
 	private static final int RANDOM_NUMBER_RANGE = 10;
+	private static final int ASCENDING_CONDITION = 5;
 
 	@Override
 	public boolean canMove() {
-		return random.nextInt(RANDOM_NUMBER_RANGE) >= 5;
+		return random.nextInt(RANDOM_NUMBER_RANGE) >= ASCENDING_CONDITION;
 	}
 }
